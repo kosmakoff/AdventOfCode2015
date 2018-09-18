3abbd9395e0a8b7365b1050ae2bbf9c7a4c0827e
Oleg Kosmakov <kosmakoff@gmail.com>
2018-09-18 18:16:04 +0200

    Days 10 and 11 complete

---
+++ Day10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using static Common.Utils;
+
+namespace Day10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader("Day 10");
+
+            const string input = "1113222113";
+
+            var answer1 = CalculateAnswer1(input);
+            var answer2 = CalculateAnswer2(input);
+
+            PrintAnswer("Answer 1", answer1);
+            PrintAnswer("Answer 2", answer2);
+        }
+
+        private static int CalculateAnswer1(string input) => CalculateAnswer(input, 40);
+        
+        private static int CalculateAnswer2(string input) => CalculateAnswer(input, 50);
+
+        private static int CalculateAnswer(string input, int iterations)
+        {
+            return Enumerable.Range(0, iterations)
+                .Aggregate(input, (s, i) => Transform(s))
+                .Length;
+        }
+
+        private static string Transform(string input)
+        {
+            var sb = new StringBuilder();
+
+            var index = 0;
+
+            while (index < input.Length)
+            {
+                var count = 0;
+
+                var charToRepeat = input[index];
+
+                do
+                {
+                    index++;
+                    count++;
+                } while (index < input.Length && charToRepeat == input[index]);
+
+                sb.Append(count);
+                sb.Append(charToRepeat);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day11/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using static Common.Utils;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader("Day 11");
+
+            const string input = "vzbxkghb";
+
+            var passwords = CalculateAnswers(input);
+
+            var answer1 = passwords[0];
+            var answer2 = passwords[1];
+
+            PrintAnswer("Answer 1", answer1);
+            PrintAnswer("Answer 2", answer2);
+        }
+
+        private static string[] CalculateAnswers(string input)
+        {
+            return EnumerateStringStarting(input)
+                .Where(IsValidPassword)
+                .Take(2)
+                .ToArray();
+        }
+
+        private static readonly Regex TwoNonOverlappingPairsRegex = new Regex(@"(?<l1>\w)\1.*(?<l2>\w)\2", RegexOptions.Compiled);
+
+        private static bool IsValidPassword(string password)
+        {
+            if (password.Contains('i') || password.Contains('o') || password.Contains('l'))
+                return false;
+
+            var match = TwoNonOverlappingPairsRegex.Match(password);
+            if (match.Success && match.Groups["l1"].Value == match.Groups["l2"].Value || !match.Success)
+                return false;
+
+            if (!HasThreeSuccessiveLetters(password))
+                return false;
+
+            return true;
+        }
+
+        private static bool HasThreeSuccessiveLetters(string password)
+        {
+            for (int i = 0; i < password.Length - 3; i++)
+            {
+                if (password[i + 1] - password[i] == 1 && password[i + 2] - password[i] == 2)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static IEnumerable<string> EnumerateStringStarting(string start)
+        {
+            yield return start;
+
+            var bytes = start.Select(c => c - 'a').ToArray();
+            var length = bytes.Length;
+
+            while (true)
+            {
+                bytes[length - 1]++;
+                for (int index = length - 1; index >= 0; index--)
+                {
+                    if (bytes[index] > 25)
+                    {
+                        bytes[index] = 0;
+                        bytes[index - 1]++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                yield return Encoding.ASCII.GetString(bytes.Select(b => (byte)('a' + b)).ToArray());
+            }
+        }
+    }
+}
