6349110de7bef2c6294a2d08ba69e83fab54e9cd
Oleg Kosmakov <kosmakoff@gmail.com>
2018-09-24 17:08:40 +0200

    Day 16 complete

---
+++ Day16/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Common.Utils;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader("Day 16");
+
+            var input = File.ReadAllLines("Input.txt")
+                .Select(SueData.Parse)
+                .ToArray();
+
+            var searchData = File.ReadAllLines("SearchData.txt")
+                .Select(x =>
+                {
+                    var parts = x.Split(": ");
+                    var name = parts[0];
+                    var count = int.Parse(parts[1]);
+                    return (Name: name, Count: count);
+                })
+                .ToDictionary(a => a.Name, a => a.Count);
+
+            var answer1 = CalculateAnswer(input, searchData, Answer1Matcher);
+            var answer2 = CalculateAnswer(input, searchData, Answer2Matcher);
+            
+            PrintAnswer("Answer 1", answer1);
+            PrintAnswer("Answer 2", answer2);
+        }
+
+        private static string CalculateAnswer(
+            IEnumerable<SueData> input,
+            Dictionary<string, int> searchData,
+            Func<KeyValuePair<string, int>, int, bool> matcher)
+        {
+            var matchingInputs = input
+                .Where(inputItem => searchData.All(sdKvp =>
+                    !inputItem.Data.TryGetValue(sdKvp.Key, out var value) || matcher(sdKvp, value)));
+
+            return matchingInputs.Single().Id;
+        }
+        
+        private static bool Answer1Matcher(KeyValuePair<string, int> kvp, int value)
+        {
+            return kvp.Value == value;
+        }
+
+        private static bool Answer2Matcher(KeyValuePair<string, int> kvp, int value)
+        {
+            switch (kvp.Key)
+            {
+                    case "cats":
+                    case "trees":
+                        return kvp.Value <= value;
+
+                    case "pomeranians":
+                    case "goldfish":
+                        return kvp.Value > value;
+
+                    default:
+                        return kvp.Value == value;
+            }
+        }
+    }
+}
---
+++ Day16/SueData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day16
+{
+    public class SueData
+    {
+        public string Id { get; }
+        public IDictionary<string, int> Data { get; }
+
+        public SueData(string id, IDictionary<string, int> data)
+        {
+            Id = id;
+            Data = data;
+        }
+
+        public static SueData Parse(string sueDataString)
+        {
+            var firstColonPosition = sueDataString.IndexOf(':');
+            var id = sueDataString.Substring(4, firstColonPosition - 4);
+
+            var remainingPart = sueDataString.Substring(firstColonPosition + 2)
+                .Split(", ")
+                .Select(x =>
+                {
+                    var parts = x.Split(": ");
+                    var name = parts[0];
+                    var count = int.Parse(parts[1]);
+                    return (Name: name, Count: count);
+                })
+                .ToDictionary(a => a.Name, a => a.Count);
+
+            return new SueData(id, remainingPart);
+        }
+    }
+}
